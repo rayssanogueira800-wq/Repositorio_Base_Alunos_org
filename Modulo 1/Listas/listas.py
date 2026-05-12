3e93944d521460aaa4897b54cbdcca7bf63b6b23
rayssanogueira800-wq <rayssanogueira800@gmail.com>
2026-05-12 08:57:25 -0300

    Add files via upload

---
+++ Modulo 1/Listas/listas.py
@@ -0,0 +1,11 @@
+nomes = ["Joaquim", "Maria", "Ana"]
+
+print(nomes[0]) # Joaquim
+print(nomes[1]) 
+print(nomes[2])
+
+nomes[0] = "João" # escreva joaquim 
+
+print(nomes[0]) # alterado para joão
+print(nomes[1])
+print(nomes[2])
